04a7ea11064059132cec2c712a77cad56b108462
Mariusz <mariusz.baran23@gmail.com>
2018-02-04 14:27:24 +0100

    Change map component to redner MAP

--- client/components/map/Map.js
+++ client/components/map/Map.js
@@ -1,9 +1,58 @@
 import React, { Component } from 'react';
 
 export default class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentLocation: {
+        lat: 50.0646501,
+        lng: 19.9449799
+      },
+      markers: [
+        {lat: 50.0646501, lng: 19.9449799},
+        {lat: 50.0646501, lng: 20.9449799}
+      ]
+    }
+
+    this.markers = [];
+  }
+  componentDidMount() {
+    const { currentLocation } = this.state;
+
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        location => this.setState({ 
+          currentLocation: {
+            lat: location.coords.latitude,
+            lng: location.coords.longitude
+          }
+        })
+      )
+    }
+    
+    this.map = new google.maps.Map(this.node, { zoom: 16, center: currentLocation });
+  }
+
+  componentWillUpdate() {
+    const { markers } = this.state;
+    this.clearOverlays();
+    markers.forEach(marker => {
+      this.markers.push(new google.maps.Marker({
+        position: marker,
+        map: this.map
+      }))
+    })
+  }
+
+  // Remove all markers from the map
+  clearOverlays() {
+    this.markers.forEach(marker => marker.setMap(null));
+    this.markers.length = 0;
+  }
+
   render() {
     return (
-      <div className="map">
+      <div ref={node => this.node = node} className="map">
 
       </div>
     )
