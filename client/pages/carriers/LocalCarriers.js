027d8d4d318fc6ad0a0cc66d5206cd8304bb42de
Mariusz <mariusz.baran23@gmail.com>
2018-02-05 19:51:12 +0100

    Create styles for Local Carriers page

--- client/pages/carriers/LocalCarriers.js
+++ client/pages/carriers/LocalCarriers.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class LocalCarriers extends Component {
   render() {
     return (
       <div className="local-carriers">
-        <h1>Local Carriers</h1>
+        <ul>
+          <li>
+            <Link to="/" className="list-item">
+              <div className="carrier-logo-container"></div>
+              <div className="carrier-description">
+                <h3 className="carrier-name">Carrier name</h3>
+                <p className="carrier-stop">Najbliższy przystanek:</p>
+              </div>
+              <div className="carrier-directions">
+                <span className="carrier-direction">KRK</span>
+                <span className="carrier-direction">DOB</span>
+              </div>
+            </Link>
+          </li>
+        </ul>
       </div>
     )
   }
