1c376f4b253e1ba235c2518eaf6a036f78b06e9d
Mariusz Baran <mariusz.baran23@gmail.com>
2018-02-05 20:51:27 +0100

    Design Local Carriers list

--- client/pages/carriers/LocalCarriers.js
+++ client/pages/carriers/LocalCarriers.js
@@ -3,22 +3,33 @@ import { Link } from 'react-router-dom';
 
 export default class LocalCarriers extends Component {
   render() {
+    const listItem = (
+      <li>
+        <Link to="/" className="list-item">
+          <div className="carrier-logo-container"></div>
+          <div className="carrier-description">
+            <h3 className="carrier-name">Travel</h3>
+            <p className="carrier-stop">Najbliższy przystanek:</p>
+          </div>
+          <div className="carrier-directions">
+            <p className="carrier-direction">KRK</p>
+            <span className="carrier-direction-indicator-both">
+              &#8691;
+            </span>
+            <p className="carrier-direction">DOB</p>
+          </div>
+        </Link>
+      </li>
+    )
+
+    const items = new Array(10).fill(listItem);
+
     return (
       <div className="local-carriers">
         <ul>
-          <li>
-            <Link to="/" className="list-item">
-              <div className="carrier-logo-container"></div>
-              <div className="carrier-description">
-                <h3 className="carrier-name">Carrier name</h3>
-                <p className="carrier-stop">Najbliższy przystanek:</p>
-              </div>
-              <div className="carrier-directions">
-                <span className="carrier-direction">KRK</span>
-                <span className="carrier-direction">DOB</span>
-              </div>
-            </Link>
-          </li>
+          {
+            items
+          }
         </ul>
       </div>
     )
