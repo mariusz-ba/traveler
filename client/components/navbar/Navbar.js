f8f24d3f4a1c4e840c1c937ebe04ff32be6b6ed4
Mariusz <mariusz.baran23@gmail.com>
2018-02-07 16:16:21 +0100

    Change navbar style

--- client/components/navbar/Navbar.js
+++ client/components/navbar/Navbar.js
@@ -5,8 +5,14 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar">
-        <Link className="navbar-brand" to="/">Traveler</Link>
-        <span className="navbar-brand-aside">Find the Route</span>
+        <span className="navbar__wprapper">
+          <Link className="navbar__brand" to="/">Traveler</Link>
+          <span className="navbar__description">Find the Route</span>
+        </span>
+        <ul className="navbar__menu">
+          <li className="navbar__link"><Link to="/signin">Sign in</Link></li>
+          <li className="navbar__link"><Link to="/signup">Sign up</Link></li>
+        </ul>
       </nav>
     )
   }
