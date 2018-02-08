a3f8ad3fb8a07a3077a935a9c72cb81ae68e7bb0
Mariusz Baran <mariusz.baran23@gmail.com>
2018-02-08 20:55:49 +0100

    Combine stopsReducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import carriersReducer from './carriersReducer';
 import mapReducer from './mapReducer';
+import stopsReducer from './stopsReducer';
 
 export default combineReducers({
   auth: authReducer,
   carriers: carriersReducer,
-  map: mapReducer
+  map: mapReducer,
+  stops: stopsReducer
 });
